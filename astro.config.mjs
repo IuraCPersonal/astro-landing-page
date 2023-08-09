7550ffd0120b29fb0f1403d87a43bd9d3ee5b908
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-08-08 23:58:14 -0400

    chore(dev): setup bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
+import bookshop from '@bookshop/astro-bookshop'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [react(), image(), bookshop()]
 })
