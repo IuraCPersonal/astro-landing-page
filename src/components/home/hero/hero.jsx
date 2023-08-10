95b2a10f2f44b8072b10d26340eba2a5c9b8d1a5
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-08-10 03:00:15 -0400

    feat(pages): added slug and index page

---
+++ src/components/home/hero/hero.jsx
@@ -0,0 +1,21 @@
+import MarkdownIt from "markdown-it";
+const md = new MarkdownIt({ html: true });
+
+export default function HomeHero(block) {
+    return (
+        <section>
+            <h3
+                className="my-5"
+                dangerouslySetInnerHTML={{
+                    __html: md.render(block.title),
+                }}
+            ></h3>
+            <p
+                dangerouslySetInnerHTML={{
+                    __html: md.render(block.description)
+                }}
+            >
+            </p>
+        </section>
+    );
+}
---
+++ src/content/pages/index.md
@@ -0,0 +1,19 @@
+---
+_schema: default
+title: Home
+seo:
+  page_description:
+  canonical_url:
+  featured_image:
+  featured_image_alt:
+  author_twitter_handle:
+  open_graph_type:
+  no_index: false
+content_blocks:
+  - _bookshop_name: home/hero
+    title: Web Dev Agency
+    description: >-
+      We help clients bring their ideas to life and foster growth in the digital realm.
+    image: /images/hero/hero-image-2.png
+    image_alt: Reports dashboard
+---
